c70c917172a3c8bd90d8d6b836b9c822834b4321
Hubert Bujakowski <hbujakow@gmail.com>
2022-10-22 14:16:26 +0200

    optimize code, add constraints

--- scraper/all.py
+++ scraper/all.py
@@ -5,8 +5,8 @@ from bs4 import BeautifulSoup
 
 urls = []
 pdf_links = []
-filters = ['kiid', 'kluczowe', 'inwestycja', 'inwestycyjne', 'inwestor', 'inwestorów', 'dokument']
-
+# filters = ['kiid', 'kluczowe', 'inwestycja', 'inwestycyjne', 'inwestor', 'inwestorów', 'dokument']
+filters = ['pdf']
 
 def get_base_url(url: str) -> str:
     start = 'h'
@@ -14,26 +14,30 @@ def get_base_url(url: str) -> str:
     base_url = url[url.find(start):url.find('/', url.find(http) + len(http), len(url))]
     return base_url
 
+def check(url):
+    if url.find('.pl')!= -1:
+        if url.find('.pl/') == -1:
+            return False
+    return True
 
 def extract_kiid_files_from_url(base_url: str, url: str) -> None:
+    if not check(url):
+        return
     try:
         read = requests.get(url)
-    except Exception:
+    except requests.exceptions:
         return
 
     html_content = read.content
     soup = BeautifulSoup(html_content, "html.parser")
-    links = [a for a in soup.select("a[href$='.pdf']")]
 
-    for link in links:
+    for link in soup.select("a[href$='.pdf']"):
         str_link = str(link).lower()
         str_link = str_link if str_link.endswith('/') else '/' + str_link
-        for f in filters:
-            if f in str_link:
-                href = link['href'] if link["href"].startswith('/') else str('/' + link['href'])
-                pdf_links.append(base_url + href)
-                print(base_url + href)
-                break
+        if any(filter in str_link for filter in filters):
+            href = link['href'] if link["href"].startswith('/') else str('/' + link['href'])
+            pdf_links.append(base_url + href)
+            print(base_url + href)
 
 
 def generate_subpages(base_url, url, depth):
@@ -44,7 +48,6 @@ def generate_subpages(base_url, url, depth):
         return
     soup = BeautifulSoup(response.text, "html.parser")
 
-    # for link in soup.select("a[href$='.html']"):
     for link in soup.find_all("a"):
         if link.get('href') is None:
             continue
@@ -53,14 +56,19 @@ def generate_subpages(base_url, url, depth):
         else:
             temp = url + link['href']
         if not temp in urls and temp.find(base_url) != -1:
-            urls.append(temp)
+            urls.append(correct_url(temp))
             # print(temp)
-        # urls.append(temp)
         generate_subpages(base_url, temp, depth - 1)
 
 
 def stripp(raw):
     return list(map(lambda url: url[:-1] if url[-1] == '/' else url, raw))
+
+def correct_url(url):
+    if url[-1] != '/':
+        return url + '/'
+    return url
+
 
 def extract_all(sites, depth=1):
     sites = stripp(sites)
@@ -73,5 +81,7 @@ def extract_all(sites, depth=1):
 
 
 if __name__ == '__main__':
-    sites = ['http://www.caspartfi.pl/']
+    # sites = ['http://www.caspartfi.pl/']
+    # sites = ['https://www.millenniumtfi.pl/', 'https://agiofunds.pl/']
+    sites = ['https://agiofunds.pl/']
     extract_all(sites, 2)
