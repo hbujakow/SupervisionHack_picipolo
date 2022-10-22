8bb3ddb6091acba00477c1514b02c09533b362d9
Hubert Bujakowski <hbujakow@gmail.com>
2022-10-22 21:41:17 +0200

    add word valid

---
+++ required_words/utils/word_validator.py
@@ -0,0 +1,44 @@
+import fitz
+from fuzzywuzzy import fuzz
+
+from pathlib import Path
+import re
+
+alphabets = "([A-Za-z])"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov)"
+digits = "([0-9])"
+
+
+def pdfToString(file):
+    text = ""
+    with fitz.open(Path(__file__).resolve().parents[1].joinpath('data', 'input', f'{file}.pdf')) as doc:
+        for page in doc:
+            text += page.get_text()
+
+    return text
+
+
+def split_into_sentences(text):
+    text = " " + text + "  "
+    text = text.replace("\n", " ")
+    text = re.sub(websites, "<prd>\\1", text)
+    text = re.sub(digits + "[.]" + digits, "\\1<prd>\\2", text)
+    if "..." in text: text = text.replace("...", "<prd><prd><prd>")
+    if "Ph.D" in text: text = text.replace("Ph.D.", "Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] ", " \\1<prd> ", text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]", "\\1<prd>\\2<prd>\\3<prd>", text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]", "\\1<prd>\\2<prd>", text)
+    text = re.sub(" " + alphabets + "[.]", " \\1<prd>", text)
+    if "”" in text: text = text.replace(".”", "”.")
+    if "\"" in text: text = text.replace(".\"", "\".")
+    if "!" in text: text = text.replace("!\"", "\"!")
+    if "?" in text: text = text.replace("?\"", "\"?")
+    text = text.replace(".", ".<stop>")
+    text = text.replace("?", "?<stop>")
+    text = text.replace("!", "!<stop>")
+    text = text.replace("<prd>", ".")
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    return sentences
