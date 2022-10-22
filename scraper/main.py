e93775ed1864996336f5d163fec926fde144f8b6
Hubert Bujakowski <hbujakow@gmail.com>
2022-10-22 16:10:47 +0200

    restructure

--- scraper/main.py
+++ scraper/main.py
@@ -1,34 +1,95 @@
-import os
+from pathlib import Path
+from typing import List
+
 import requests
-from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
-import pdf_reader
+import utils.utils as utils
 
-raw = ['https://www.aliortfi.com/', 'https://www.amundi.pl/Inwestorzy_indywidualni']
+data_path = Path(__file__).resolve().parents[0].joinpath('data', 'websites.txt')
+urls = []
+pdf_links = []
+filters = ['kiid', 'kluczowe', 'inwestycja', 'inwestycyjne', 'inwestor', 'inwestorów', 'dokument']
 
 
-def strip(raw):
-    return list(map(lambda url: url[:-1] if url[-1] == '/' else url, raw))
+def extract_kiid_files_from_url(base_url: str, url: str) -> None:
+    """
+    Extracts only KIID related pdf files from given url
+    :param base_url: base url of webpage
+    :param url: some subpage of base_url
+    """
+    if not utils.check(url):
+        return
+    try:
+        read = requests.get(url)
+    except requests.exceptions:
+        return
+    if not read.ok:
+        return
+    html_content = read.content
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    for link in soup.select("a[href$='.pdf']"):
+        str_link = str(link).lower()
+        str_link = str_link if str_link.endswith('/') else '/' + str_link
+        if any(filter in str_link for filter in filters):
+            href = link['href'] if link["href"].startswith('/') else str('/' + link['href'])
+            pdf_links.append(base_url + href)
+            print(base_url + href)
 
 
-def generate_subpages(url, depth=1):
-    response = requests.get(url)
+def generate_subpages(base_url: str, url: str, depth: int) -> None:
+    """
+    Generate subpage from given url
+    :param base_url: global base url
+    :param url: subpage of base_url
+    :param depth: how much depth we need (recursive)
+    """
+    if depth == 0:
+        return
+    if utils.check(url):
+        return
+    try:
+        response = requests.get(url)
+    except requests.exceptions.InvalidURL:
+        return
+    if not response.ok:
+        return
     soup = BeautifulSoup(response.text, "html.parser")
 
-    return list(set([link['href'] if link['href'].__contains__('http') else url + link['href'] for link in
-                     soup.select("a[href$='.html']")]))
+    for link in soup.find_all("a"):
+        if link.get('href') is None:
+            continue
+        if link['href'].__contains__('http'):
+            temp = link['href']
+        else:
+            temp = url + link['href']
+        if not temp in urls and temp.find(base_url) != -1:
+            urls.append(temp)
+            print(temp)
+        generate_subpages(base_url, temp, depth - 1)
 
 
-def main():
-    # urls = strip(raw)
-    url = 'http://www.millenniumtfi.pl'
+def extract_all(sites: List[str], depth: int = 1) -> None:
+    sites = utils.stripp(sites)
+    for site in sites:
+        urls.clear()
+        generate_subpages(site, site, depth)
+        for url in urls:
+            extract_kiid_files_from_url(site, url)
+    # print(pdf_links)
 
-    # print(generate_subpages(url))
 
-    print(pdf_reader.list_all_pdfs(url, url))
-    # pdf_reader.download_single_pdf('http://www.millenniumtfi.pl/dl/5796/attachment/70ec21/Komentarz_18.10.2022.pdf')
+def download_all_pdfs() -> None:
+    i = 0
+    for pdf in list(set(pdf_links)):
+        utils.download_pdf(pdf, i)
+        i += 1
 
 
 if __name__ == '__main__':
-    main()
+    # webpages = utils.read_websites_from_file(data_path)
+
+    sites = ['https://www.caspar.com.pl/']  ### example
+    extract_all(sites, 1)
+    download_all_pdfs()
