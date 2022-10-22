3f3030d35129dcc488f87666e6c12bad6ebe292a
Hubert Bujakowski <hbujakow@gmail.com>
2022-10-22 13:40:20 +0200

    remove unused, add exc

--- scraper/all.py
+++ scraper/all.py
@@ -2,7 +2,6 @@ from typing import List
 
 import requests
 from bs4 import BeautifulSoup
-from re import search
 
 urls = []
 pdf_links = []
@@ -12,13 +11,16 @@ filters = ['kiid', 'kluczowe', 'inwestycja', 'inwestycyjne', 'inwestor', 'inwest
 def get_base_url(url: str) -> str:
     start = 'h'
     http = '://'
-    end = '/'
     base_url = url[url.find(start):url.find('/', url.find(http) + len(http), len(url))]
     return base_url
 
 
-def extract_kiid_files_from_url(pdf_links: List[str], base_url: str, url: str) -> None:
-    read = requests.get(url)
+def extract_kiid_files_from_url(base_url: str, url: str) -> None:
+    try:
+        read = requests.get(url)
+    except Exception:
+        return
+
     html_content = read.content
     soup = BeautifulSoup(html_content, "html.parser")
     links = [a for a in soup.select("a[href$='.pdf']")]
@@ -30,13 +32,16 @@ def extract_kiid_files_from_url(pdf_links: List[str], base_url: str, url: str) -
             if f in str_link:
                 href = link['href'] if link["href"].startswith('/') else str('/' + link['href'])
                 pdf_links.append(base_url + href)
+                print(base_url + href)
                 break
 
 
-def generate_subpages(base_url, url, limit):
-    if limit == 0:
+def generate_subpages(base_url, url, depth):
+    if depth == 0:
         return
     response = requests.get(url)
+    if not response.ok:
+        return
     soup = BeautifulSoup(response.text, "html.parser")
 
     # for link in soup.select("a[href$='.html']"):
@@ -47,22 +52,26 @@ def generate_subpages(base_url, url, limit):
             temp = link['href']
         else:
             temp = url + link['href']
-        if not temp in urls and temp.find(base_url)!=-1:
+        if not temp in urls and temp.find(base_url) != -1:
             urls.append(temp)
             # print(temp)
-        urls.append(temp)
-        generate_subpages(base_url, temp, limit - 1)
+        # urls.append(temp)
+        generate_subpages(base_url, temp, depth - 1)
 
 
 def stripp(raw):
     return list(map(lambda url: url[:-1] if url[-1] == '/' else url, raw))
 
+def extract_all(sites, depth=1):
+    sites = stripp(sites)
+    for site in sites:
+        urls.clear()
+        generate_subpages(site, site, depth)
+        print(urls)
+        for url in urls:
+            extract_kiid_files_from_url(get_base_url(url), url)
+
 
 if __name__ == '__main__':
-    site = 'http://www.noblefunds.pl'
-    generate_subpages(site, site, 1)
-    for url in urls:
-        base_url = get_base_url(url)
-        extract_kiid_files_from_url(pdf_links, base_url, url)
-    print(pdf_links)
-    print(len(pdf_links))
+    sites = ['http://www.caspartfi.pl/']
+    extract_all(sites, 2)
