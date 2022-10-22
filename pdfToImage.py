4f4dea4025b0adede42f63743f0f2ac9763df687
galkowskim <mikolaj.galkowski7@gmail.com>
2022-10-22 15:26:25 +0200

    add pdf2image

--- pdfToImage.py
+++ pdfToImage.py
@@ -1,7 +1,7 @@
 import os
 from pdf2image import convert_from_path
 
-ROOT = './dokumenty-labelowanie/'
+ROOT = './documents/'
 
 idx = 1
 
@@ -9,6 +9,6 @@ for file in os.listdir(ROOT):
     images = convert_from_path(os.path.join(ROOT, file), poppler_path=r'C:\Users\mikol\Downloads\poppler-22.04.0\Library\bin')
     image0 = images[0]
     image1 = images[1]
-    image0.save(f'./zdjecia/image_{idx}_0.png')
-    image1.save(f'./zdjecia/image_{idx}_1.png')
+    image0.save(f'./images/image_{idx}_0.png')
+    image1.save(f'./images/image_{idx}_1.png')
     idx += 1
